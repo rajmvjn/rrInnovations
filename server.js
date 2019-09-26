929693d43918f0ee3e9cb39ff7619d2092065afb
rajmvjn <rajmvjn@gmail.com>
2019-09-26 22:43:53 +0530

    heroku deployment

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 
 connectDB();
-const PORT = process.env.PORT || 5000; // on the server it takes from PORT, on dev it takes 5000
+// on the server it takes from PORT, on dev it takes 5000
 
 //initialize middleware
 app.use(express.json({extended: false}));
-
-app.get('/', (req, res) => res.send('App running'));
 
 //routes
 app.use('/api/user', require('./routes/api/user'));
@@ -16,4 +15,15 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 
+//static asset in production
+if( process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static(client/build));
+
+    app.get( '*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => console.log(`Server started on the port ${PORT}`));
